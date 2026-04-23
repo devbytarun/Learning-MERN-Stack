d1dff88d8037a36f362ea216d139831da811c19b
devbytarun <devbytarun@gmail.com>
2026-04-23 09:29:27 +0530

    feat: middleware complete — morgan, cors, helmet, rate-limit

--- MongoDB_Basics/app.js
+++ MongoDB_Basics/app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,  // 15 minutes
+  max: 100                     // 100 requests max
+})
 
 const productRoutes = require('./routes/productRoutes');
 
-const app = express();
-app.use(express.json());
+const app = express();  
+
+// phir middlewares
+app.use(helmet())
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(limiter)
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB Connected!'))
@@ -18,4 +33,4 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));             
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
